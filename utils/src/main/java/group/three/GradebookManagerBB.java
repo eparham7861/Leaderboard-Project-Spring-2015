e01678370635fbc054b06051b2d36d1fa8d992f8
eparham7861 <eparham7861@hotmail.com>
2015-02-11 11:48:43 -0600

    Add test code for GradebookManager

---
+++ utils/src/main/java/group/three/GradebookManagerBB.java
@@ -0,0 +1,5 @@
+package group.three;
+
+public class GradebookManagerBB {
+
+}
---
+++ utils/src/test/java/group/three/GradebookManagerBBTest.java
@@ -0,0 +1,19 @@
+package group.three;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+import blackboard.platform.gradebook2.*;
+import blackboard.platform.gradebook2.impl.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class GradebookManagerBBTest {
+	private GradebookManagerBB currentGradebook;
+	
+	@Before
+	public void startUp() {
+		GradebookManager mockGradebook = mock(GradebookManager.class);
+				
+		currentGradebook = new GradebookManagerBB();
+	}
+}
